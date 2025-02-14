76b8535080ec10cbc693398f39c219d6fad693c0
azzahrahafizhah <azzahra.hafizhah@student.alhazen.academy>
2025-02-14 13:00:33 +0700

    Add files via upload

---
+++ Phyton.py
@@ -0,0 +1,15 @@
+print("Formulir Pendaftaran Akun Siswa")
+print("==========================")
+
+name = input("Nama Lengkap: ")
+email = input("Email: ")
+nomor = input("Nomor Handphone: ")
+
+print("Hi", name )
+print("Selamat! Email", email, "berhasil terdaftar di sistem kami")
+
+print("link untuk login ke aplikasi dikirim ke nomor", Nomor)
+
+
+
+
