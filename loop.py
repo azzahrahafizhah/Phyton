6c3b6b19f903c2a2d5ee3cac229e610724233088
azzahrahafizhah <azzahra.hafizhah@student.alhazen.academy>
2025-05-16 10:32:47 +0700

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,22 @@
+score = 75
+score = (int);score
+min_criteria = 75
+
+if score == min_criteria:
+    print("Anda berhasil naik level!")
+if score >= min_criteria:
+   print("Anda berhasil naik level!")
+elif score < min_criteria:
+    print("Anda belum berhasil naik level!")
+ 
+ # loopwhile
+ number = 10
+
+while number <= 10:
+    print("I like phyton")
+ numbers = range(10)
+#  n merupakan variable baru
+# yang akan menyimpan masing masing item
+# dari iterable object
+# for n in numbers
+
---
+++ loop2.py
@@ -0,0 +1,12 @@
+#for
+for i in range(1, 6):
+    print(i)
+
+#while
+jumlah = input("input count = ")
+jumlah = int(jumlah)
+
+count = 1
+while count <= jumlah:
+    print("tes")
+    count += 1
