6a1a2a94866943a37e6d851b1c146ad32b95517a
azzahrahafizhah <azzahra.hafizhah@student.alhazen.academy>
2025-10-17 13:31:33 +0700

    Add files via upload

---
+++ pip.py
@@ -0,0 +1,7 @@
+from stringcolor import *
+
+print(cs("Selamat Datang", "purple", "blue"))
+print(bold("Selamat Datang"))
+print(underline("Selamat Datang"))
+print(underline("Selamat Datang").bold())
+print(cs("Selamat Datang", "pink", "grey").underline())
